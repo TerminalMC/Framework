15dfdcb55495ed851bc0c74a87165abc52200879
NotRyken <127091011+NotRyken@users.noreply.github.com>
2024-10-18 10:11:40 +0800

    Add license notice and gradle task

--- neoforge/src/main/java/dev/terminalmc/framework/mixin/MixinTitleScreen.java
+++ neoforge/src/main/java/dev/terminalmc/framework/mixin/MixinTitleScreen.java
@@ -1,3 +1,14 @@
+/*
+ * Framework by TerminalMC
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * Framework has waived all copyright and related or neighboring rights
+ * to Framework.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
 package dev.terminalmc.framework.mixin;
 
 import dev.terminalmc.framework.Framework;
