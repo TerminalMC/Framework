389be76b9c622ba161d4666847465483adf9d025
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-06-19 17:18:41 +0800

    Standard mixin naming

--- common/src/main/java/dev/terminalmc/framework/mixin/MinecraftMixin.java
+++ common/src/main/java/dev/terminalmc/framework/mixin/MinecraftMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraft {
+public class MinecraftMixin {
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void init(CallbackInfo info) {
--- fabric/src/main/java/dev/terminalmc/framework/mixin/TitleScreenMixin.java
+++ fabric/src/main/java/dev/terminalmc/framework/mixin/TitleScreenMixin.java
@@ -15,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class MixinTitleScreen {
+public class TitleScreenMixin {
 
     @Inject(method = "init()V", at = @At("HEAD"))
     private void init(CallbackInfo info) {
