7294d3514b2b20842c91238cae2fad64456c7615
NotRyken <127091011+NotRyken@users.noreply.github.com>
2026-05-10 08:56:03 +0800

    Add tag cleanup workflow

---
+++ .github/workflows/scripts/cleanup-tags.js
@@ -0,0 +1,40 @@
+module.exports = async ({github, context, core}) => {
+    const {owner, repo} = context.repo;
+    core.info(`Running script in ${owner}/${repo}`);
+
+    const releaseTag = context.payload.release.tag_name;
+
+    console.log(`Checking remaining releases for tag: ${releaseTag}`);
+
+    // Fetch all releases
+    const releases = await github.paginate(
+            github.rest.repos.listReleases,
+            {
+                owner,
+                repo,
+                per_page: 100
+            }
+    );
+
+    // Find any release still using this tag
+    const existingRelease = releases.find(r => r.tag_name === releaseTag);
+    if (existingRelease) {
+        console.log(`Tag is still used by release ${existingRelease.name}.`);
+        return;
+    }
+
+    console.log(`No releases found. Deleting tag...`);
+
+    try {
+        await github.rest.git.deleteRef({
+            owner,
+            repo,
+            ref: `tags/${releaseTag}`
+        });
+
+        console.log(`Deleted tag ${releaseTag}`);
+    } catch (err) {
+        console.log(`Failed to delete tag: ${err.message}`);
+        core.setFailed(err.message);
+    }
+}
